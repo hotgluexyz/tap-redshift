dfcce09e8a7c6a9740743ba3e13bef071f1444f9
Adil Ahmed <xac.adil@gmail.com>
2024-01-16 21:16:48 +0500

    Fix for parsing config start_date format from the HG

--- tap_redshift/__init__.py
+++ tap_redshift/__init__.py
@@ -406,8 +406,7 @@ def sync_table(connection, catalog_entry, state,query=None):
         params = {}
 
         if start_date is not None:
-            formatted_start_date = datetime.datetime.strptime(
-                start_date, '%Y-%m-%dT%H:%M:%SZ').astimezone()
+            formatted_start_date = datetime.datetime.strptime(start_date, '%Y-%m-%dT%H:%M:%S.%fZ').astimezone()
 
         replication_key = metadata.to_map(catalog_entry.metadata).get(
             (), {}).get('replication-key')
