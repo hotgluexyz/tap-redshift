aa5f6b9545d142bbfe89d0c5a0104e3fa2d8181c
Azmat Ali <mian.azmatali.9@gmail.com>
2025-05-13 15:45:08 -0400

    Refactor cursor handling in sync_table function to prevent potential leaks

--- tap_redshift/__init__.py
+++ tap_redshift/__init__.py
@@ -406,7 +406,9 @@ def sync_table(connection, catalog_entry, state,query=None):
 
     tap_stream_id = catalog_entry.tap_stream_id
     LOGGER.info('Beginning sync for {} table'.format(tap_stream_id))
-    with connection.cursor(f"redshift_cursor_{secrets.token_hex(8)}") as cursor:
+    # TODO: Keep this logic for now, but remove it once we verify that the cursor is not leaking
+    # with connection.cursor(f"redshift_cursor_{secrets.token_hex(8)}") as cursor:
+    with connection.cursor() as cursor:
         if not query:
             schema, table = catalog_entry.table.split('.')
             database = catalog_entry.database
